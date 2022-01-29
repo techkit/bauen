9400c4efaa56215abe9cef1504b7f9f243d9f0cc
rrosas1198 <rrosas1198@gmail.com>
2022-01-29 18:44:45 -0500

    refactor: load package cache added

--- src/utils/load-package.util.ts
+++ src/utils/load-package.util.ts
@@ -2,7 +2,16 @@ import { readFile } from "fs/promises";
 import { resolve } from "pathe";
 import { PackageJson } from "../interfaces/package-json.interface";
 
-export async function loadPackage(rootDir: string) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let packageJson: PackageJson = {} as any;
+
+export async function loadPackage(rootDir: string, force?: boolean) {
+    if (packageJson.name && !force) {
+        return Promise.resolve(packageJson);
+    }
+
     const raw = await readFile(resolve(rootDir, "package.json"));
-    return JSON.parse(raw.toString()) as PackageJson;
+    packageJson = JSON.parse(raw.toString()) as PackageJson;
+
+    return Promise.resolve(packageJson);
 }
